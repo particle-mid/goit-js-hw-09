c18e29f01becd55bcf2b76393bd925753d4176b2
particle-mid <115956474+particle-mid@users.noreply.github.com>
2023-03-21 22:03:31 +0000

    Deploying to gh-pages from @ particle-mid/goit-js-hw-09@94f6b39038f8142e57cc40ad1ca5e18e84dd55a9 🚀

---
+++ 01-color-switcher.5b6e35b1.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyColor:document.querySelector("body")};let e=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,e=setInterval((()=>{t.bodyColor.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.5b6e35b1.js.map
